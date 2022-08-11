6575c3dab5f05d59be85fcdaf16efd74378f6ced
Sumanta Saha <saha.sumanta8@gmail.com>
2022-08-12 01:23:33 +0530

    Added logic to parse CSV

--- Client/src/com/sumanta/mytool/Starter.java
+++ Client/src/com/sumanta/mytool/Starter.java
@@ -1,10 +1,12 @@
 package com.sumanta.mytool;
 
+import com.sumanta.mytool.service.CsvUtility;
 import com.sumanta.mytool.service.UnzipUtility;
 
 import java.nio.file.Path;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Starter {
     public Starter() {
@@ -22,6 +24,7 @@ public class Starter {
         System.out.println("Starting application...");
         UnzipUtility unzipUtility = new UnzipUtility();
         
+        /// COPY FILES TO TEMP
         System.out.println("Copying files...");
         ArrayList<Path> filePaths = unzipUtility.copyFilesToTemp(directory);
         
@@ -33,13 +36,24 @@ public class Starter {
             // this bug needs to be fixed
             filePaths.remove(0);
             
+            /// EXTRACT FILES
             System.out.println("Extracting files...");
-            unzipUtility.extractFiles(filePaths);
+            ArrayList<Path> outputPaths = unzipUtility.extractFiles(filePaths);
             System.out.println("Extraction complete.");
+                        
+            /// PARSE CSV
+            CsvUtility csvUtility = new CsvUtility();
+            for(Path outputPath: outputPaths){
+                System.out.printf("Reading %s.", outputPath);
+                System.out.println();
+                List<List<String>> data = csvUtility.readCsv(outputPath);
+                System.out.println((data.size()-1) + " records found.");
+            }
+            
             
             // Optional- used for testing
             // Open temp folder which stores all data
-            unzipUtility.openWorkspace();
+            //unzipUtility.openWorkspace();
         }
     }
 }
--- Client/src/com/sumanta/mytool/utility/Constants.java
+++ Client/src/com/sumanta/mytool/utility/Constants.java
@@ -9,4 +9,6 @@ public final class Constants {
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd-HHmmss.SSS";
     public static final String ZIP_INPUT_FOLDER_NAME = "Raw";
     public static final String ZIP_EXTRACTED_FOLDER_NAME = "Extracted";
+    public static final String METADATA_FILENAME = "Metadata.csv";
+    public static final String COMMA_DELIMITER = ",";
 }
